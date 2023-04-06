3202063b5b63bff6295c5f569a34ad86b9918025
RoskiDeluge <roberto.delgado@gmail.com>
2023-04-06 13:29:22 -1000

    Header updated

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    domains: ["aleph-m-2.s3.amazonaws.com", "fakestoreapi.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
